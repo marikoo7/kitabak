be8290613e0918c9e545fa65aeed89715f11d7f0
salmamal <salmamedhat637@gmail.com>
2025-03-22 01:34:35 +0200

    about page

--- kitabak/app/(tabs)/about.jsx
+++ kitabak/app/(tabs)/about.jsx
@@ -1,10 +1,32 @@
-import  { Text, View ,TextInput, StyleSheet, ScrollView } from "react-native";
+import { Text, View ,TextInput, StyleSheet, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
+import ProfilePic from "@/components/profilePic";
+import SearchBar from "@/components/searchBar";
+import SearchResult from "@/components/searchResult";
 
 export default function AboutScreen() {
+  const [user, setUser] = useState({
+    loggedIn: false,
+    profilePic: "https://example.com/user-profile.jpg",
+  });
+  const [books, setBooks] = useState([]);
+  const [searchPerformed, setSearchPerformed] = useState(false);
+
   return (
     
     <ScrollView style={styles.container}>
+      <View style={styles.profileContainer}>
+              <ProfilePic uri={user.loggedIn ? user.profilePic : null} size={80} />
+            </View>
+      
+            <View style={styles.searchContainer}>
+              <SearchBar onSearch={setBooks} setSearchPerformed={setSearchPerformed} />
+            </View>
+      
+            <View style={styles.searchResult}>
+              <SearchResult books={books} searchPerformed={searchPerformed} />
+            </View>
       <Text style={styles.title}>Happy reading,</Text>
       <Text style={styles.title}>with kitabak</Text>
       <Text style={styles.paragraph}>Welcome to Kitabak, your personal digital library designed for seamless reading and book management. Inspired by the best in the industry, Kitabak offers an intuitive and immersive experience for book lovers, making it easy to discover, organize, and access your books on multiple devices, ensuring a continuous reading journey.
@@ -23,14 +45,27 @@ export default function AboutScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 40,
+    padding: 10,
+  },
+  profileContainer: {
+    position: "absolute",
+    top: 33,
+    right: 20,
+  },
+  searchContainer: {
+    top: 35,
+    left: 10,
+  },
+  searchResult: {
+    flex: 1,
+    marginTop: 40,
+    paddingHorizontal: 10,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
     color:"#585047",
-
   },
   paragraph: {
     fontSize: 16,
