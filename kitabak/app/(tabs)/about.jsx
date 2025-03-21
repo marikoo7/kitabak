d630904895d45573a0a7940a26b80d3dcfae6e66
salmamal <salmamedhat637@gmail.com>
2025-03-21 21:39:32 +0200

    about page

--- kitabak/app/(tabs)/about.jsx
+++ kitabak/app/(tabs)/about.jsx
@@ -1,4 +1,4 @@
-import { Text, View ,TextInput, StyleSheet, ScrollView } from "react-native";
+import  { Text, View ,TextInput, StyleSheet, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function AboutScreen() {
