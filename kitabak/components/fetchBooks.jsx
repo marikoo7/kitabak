c9b60341e588a0690c475ca49323940b8dda0604
maimoh <maimoh85349@gmail.com>
2025-05-08 21:46:28 +0300

    Update fetchBooks.jsx

--- kitabak/components/fetchBooks.jsx
+++ kitabak/components/fetchBooks.jsx
@@ -2,7 +2,10 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../kitabak-server/firebaseConfig"; 
 
 export const fetchBooks = async () => {
-  const booksRef = collection(db, "books");
-  const snapshot = await getDocs(booksRef);
-  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  const snapshot = await getDocs(collection(db, "books"));
+  return snapshot.docs.map(doc => ({
+    id: doc.id,
+    title: doc.data().title.toLowerCase(), 
+    description: doc.data().description,
+  }));
 };
